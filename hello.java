4499b3af767cf13f6fe52b738c56b2fcc187e46c
Dev3 <Dev3@email.com>
2019-12-29 18:49:44 +0000

    from dev3

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
+class Simple
+{     
+public static void main(String args[])
+ {  
+     System.out.println("Hello Java from dev12:");  
     }  
 } 
